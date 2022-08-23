c534b700d1792a411e937e7fd1936f1e60c5e374
nikul1ch <nikulin@netkam.ru>
2022-08-23 18:59:20 +0300

    Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.

--- seminar2.3/Program.cs
+++ seminar2.3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите цифру от 1 до 7 которая обозначает день недели: ");
+int index = Convert.ToInt32(Console.ReadLine());
+string[] array = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+// int index = digit;
+if (index < 8 && index > 0)
+{
+    if (index == 6 || index == 7) Console.Write($"Введённая цифра {index} это {array[index - 1]} и это выходной");
+    else Console.Write($"Введённая цифра {index} это {array[index - 1]} и это не выходной");
+}
+else Console.Write($"Введённая цифра {index} меньше 1 или больше 7");
