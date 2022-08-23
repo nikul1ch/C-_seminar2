a440218b8639325639cdccfe12250be1bf928ba1
nikul1ch <nikulin@netkam.ru>
2022-08-23 18:31:07 +0300

    Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

--- seminar2.2/Program.cs
+++ seminar2.2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите число больше 99: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int number = digit;
+if (digit > 99 && digit < 1000)
+{
+    digit /= 10;
+    digit /= 10;
+    Console.Write($"Третья цифра числа {number} это {digit}");
+}
+else if (digit > 999)
+{
+    digit /= 10;
+    digit /= 10;
+    digit %= 10;
+    Console.Write($"Третья цифра числа {number} это {digit}");
+}
+else Console.Write("Третьей цифры нет");
