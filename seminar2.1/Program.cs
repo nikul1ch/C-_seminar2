e021dca88db02f424e2ac9cdab0f460784b998d2
nikul1ch <nikulin@netkam.ru>
2022-08-23 17:47:26 +0300

    Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

--- seminar2.1/Program.cs
+++ seminar2.1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите трёхзначное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int number = digit;
+if (digit > 99 && digit < 1000)
+{
+    digit /= 10;
+    digit %= 10;
+    Console.Write($"Вторая цифра числа {number} это {digit}");
+}
+else Console.Write("Число не двухзначное");
